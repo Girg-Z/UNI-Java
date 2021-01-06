b5e27358b5811aa235cb32832cf415cdcb9ac5e2
Giorgio Zanchetti <girg.zanchetti@gmail.com>
2021-01-06 12:03:56 +0100

    Init

---
+++ src/test/java/it/univpm/domainFinder/DomainFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.domainFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
