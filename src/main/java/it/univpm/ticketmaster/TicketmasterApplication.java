6f5f8e965dd9adc646af92636d35b43a2e6c3ab5
DIEGO-MANZO <diegomanzo262@gmail.com>
2021-01-18 17:03:26 +0100

    getMetaData method implementation

--- src/main/java/it/univpm/ticketmaster/TicketmasterApplication.java
+++ src/main/java/it/univpm/ticketmaster/TicketmasterApplication.java
@@ -3,11 +3,38 @@ package it.univpm.ticketmaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
+import it.univpm.ticketmaster.model.Event;
+
+
+import java.util.*;
+import java.lang.reflect.*;
+
 @SpringBootApplication
 public class TicketmasterApplication {
 
+	private static final String Z = null;
+	private static Object startdate;
+
 	public static void main(String[] args) {
+		Event e;
+		// Convert ISO 8601 dateTime String to Date Object
+			TemporalAccessor temporalAccessor = DateTimeFormatter.ISO_INSTANT.parse(
+				"2021-01-24T23:40:00Z"
+            );
+            Instant instant = Instant.from(temporalAccessor);
+            Date startDate = Date.from(instant);
+		e = new Event  ("12we","evento1","rock",startDate,startDate,"vffvfv","fvbgg");
+	//	String a= e.getMetaData();
+	//	System.out.println(e.getMetaData());
+
+		System.out.println(e.getMetaData());
+		
 		SpringApplication.run(TicketmasterApplication.class, args);
 	}
-
 }
+
+
--- src/test/java/it/univpm/ticketmaster/TicketmasterApplicationTests.java
+++ src/test/java/it/univpm/ticketmaster/TicketmasterApplicationTests.java
@@ -2,12 +2,12 @@ package it.univpm.ticketmaster;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.*;
+import java.awt.print.*;
 
 @SpringBootTest
 class TicketmasterApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
