b6c8d93259c9fdd9062d5243316fe1cfc350779a
Giorgio Zanchetti <girg.zanchetti@gmail.com>
2021-01-19 11:47:03 +0100

    Fixed Content-Type error in

--- src/main/java/it/univpm/ticketmaster/controller/ApiController.java
+++ src/main/java/it/univpm/ticketmaster/controller/ApiController.java
@@ -1,15 +1,20 @@
 package it.univpm.ticketmaster.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import it.univpm.ticketmaster.model.Event;
 
 @RestController
 public class ApiController {
 
-    @RequestMapping(value = "/metadata", method = RequestMethod.GET, headers = "Content-type=application/json")
-    public String metadata() {
-        return Event.getMetaData();
+    @GetMapping("/metadata")
+    public ResponseEntity<String> metadata() {
+        final HttpHeaders httpHeaders= new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<String>(Event.getMetaData(),httpHeaders, HttpStatus.OK);
     }
 }
